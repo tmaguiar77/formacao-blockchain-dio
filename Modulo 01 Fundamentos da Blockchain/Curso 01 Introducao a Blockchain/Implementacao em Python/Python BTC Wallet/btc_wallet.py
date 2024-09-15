84e5dab2a6d613cdc3b40df6c95fcb0e2f1f0e7c
tmaguiar <tmaguiar@gmail.com>
2024-09-15 13:52:53 -0300

    update: Correcao de derivacao

--- Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Implementacao em Python/Python BTC Wallet/btc_wallet.py
+++ Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Implementacao em Python/Python BTC Wallet/btc_wallet.py
@@ -1,9 +1,10 @@
+
 from hdwallet import HDWallet
 from hdwallet.utils import generate_entropy
 from hdwallet.symbols import BTCTEST as SYMBOL
 from typing import Optional
 
-# Escolha entre 128, 160, 192, 224 ou 256 - determinando o numero de palavras
+# Escolha entre 128, 160, 192, 224 ou 256 - determinando o numero de palavras do mnemonico
 STRENGTH: int = 128  # Default = 128
 # Escolha a linguagem english, french, italian, spanish, chinese_simplified, chinese_traditional, japanese ou korean
 LANGUAGE: str = "english"  # Default = english
@@ -12,15 +13,20 @@ ENTROPY: str = generate_entropy(strength=STRENGTH)
 # Senha da passphrase
 PASSPHRASE: Optional[str] = None
 
-# Inicializar carteira.
-hdwallet: HDWallet = HDWallet(symbol=SYMBOL, use_default_path=False)
+def main():
+    # Inicializar carteira.
+    hdwallet: HDWallet = HDWallet(symbol=SYMBOL, use_default_path=False)
 
-# Criar carteira pela entropia
-hdwallet.from_entropy(entropy=ENTROPY, language=LANGUAGE, passphrase=PASSPHRASE)
-hdwallet.from_index(49, hardened=True)
+    # Criar carteira pela entropia
+    hdwallet.from_entropy(entropy=ENTROPY, language=LANGUAGE, passphrase=PASSPHRASE)
+    hdwallet.from_path(path=f"m/49/1/0/0/0")
 
-# Exibir a mensagem final com detalhes da carteira
-print("Carteira gerada")
-print(f"Endereço: {hdwallet.p2pkh_address()}")
-print(f"Chave Privada: {hdwallet.private_key()}")
-print(f"Seed: {hdwallet.mnemonic()}")
+    # Exibir a mensagem final com detalhes da carteira
+    print(f"Carteira Gerada")
+    print(f"===============\n")
+    print(f"Endereço: {hdwallet.p2pkh_address()}\n")
+    print(f"Chave Privada WIF: {hdwallet.wif()}\n")
+    print(f"Seed: {hdwallet.mnemonic()}")
+
+if __name__ == "__main__":
+    main()
